e6548077741fced33c35231884af5aa7b69b1432
MichelDevWeb <micheldevweb2020@gmail.com>
2021-07-26 09:53:53 +0700

    generated deployables

--- functions/src/index.ts
+++
@@ -1,4 +0,0 @@
-import * as functions from 'firebase-functions';
-const universal = require(`${process.cwd()}/dist/pk2-angular/server/main.js`).app;
-
-export const ssr = functions.https.onRequest(universal);
